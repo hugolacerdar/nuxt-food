4f6d1523f7f9442fd40e960f06255b9062fbd7b1
Hugo <hugolacerdar@gmail.com>
2022-01-17 14:03:43 -0300

    Add index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <Tutorial/>
+  <main class="container">
+    <AppHeader />
+    <AppRestaurantInfo />
+  </main>
 </template>
 
 <script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+
 export default {
-  name: 'IndexPage'
-}
+  components: {
+    AppRestaurantInfo,
+    AppHeader,
+  },
+};
 </script>
