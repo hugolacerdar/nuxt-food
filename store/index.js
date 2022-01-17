5a65bdf627bbd5f232cb9179a212cfe8335827b6
Hugo <hugolacerdar@gmail.com>
2022-01-17 14:04:25 -0300

    Add store

---
+++ store/index.js
@@ -0,0 +1,38 @@
+export const state = () => ({
+  foodData: [],
+});
+
+// export const getters = {
+//     getterValue: state => {
+//         return state.value
+//     }
+// }
+
+export const mutations = {
+  updateFoodData: (state, data) => {
+    state.foodData = data;
+  },
+};
+
+export const actions = {
+  async getFoodData({ state, commit }) {
+    if (state.foodData.length) return;
+    try {
+      await fetch(
+        "https://dva9vm8f1h.execute-api.us-east-2.amazonaws.com/production/restaurants",
+        {
+          headers: {
+            "Content-type": "application/json",
+            "x-api-key": process.env.AWS_API_KEY,
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          commit("updateFoodData", data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
