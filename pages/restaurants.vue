170138befbb15b344af958223d4512fdd8f7e3ff
Hugo <hugolacerdar@gmail.com>
2022-01-17 14:03:52 -0300

    Add restaurants page

---
+++ pages/restaurants.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="container restaurant">
+    <h1 class="title">Restaurants:</h1>
+    <AppRestaurantInfo />
+  </main>
+</template>
+
+<script>
+import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+
+export default {
+  components: {
+    AppRestaurantInfo,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  padding: 20px 200px;
+}
+</style>
