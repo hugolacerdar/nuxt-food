801c79fa0e281b26c3b4cd21dbcaa2b40c64fda6
Hugo <hugolacerdar@gmail.com>
2022-01-18 19:11:49 -0300

    Add app select component

--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,16 +1,40 @@
 <template>
   <main class="container restaurant">
-    <h1 class="title">Restaurants:</h1>
-    <AppRestaurantInfo />
+    <div class="restaurantheading">
+      <h1>Restaurants:</h1>
+      <AppSelect @change="selectedRestaurant = $event" />
+    </div>
+    <AppRestaurantInfo :dataSource="filteredRestaurants" />
   </main>
 </template>
 
 <script>
 import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+import AppSelect from "@/components/AppSelect.vue";
+import { mapState } from "vuex";
 
 export default {
   components: {
     AppRestaurantInfo,
+    AppSelect,
+  },
+  data() {
+    return {
+      selectedRestaurant: "",
+    };
+  },
+  computed: {
+    ...mapState(["foodData"]),
+    filteredRestaurants() {
+      if (this.selectedRestaurant) {
+        return this.foodData.filter((el) => {
+          let name = el.name.toLowerCase();
+          return name.includes(this.selectedRestaurant);
+        });
+      }
+
+      return this.foodData;
+    },
   },
 };
 </script>
